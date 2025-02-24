315815b26a47f968fbdd227670f684d5f1674e55
Wingscape <wingscape17@gmail.com>
2025-02-24 16:42:24 +0700

    Added staging to dbt

---
+++ dbt/urban_transform/models/staging/raw/stg_raw__world_banks.sql
@@ -0,0 +1,17 @@
+WITH source AS (
+    SELECT * FROM {{ source('raw', 'data_source') }}
+),
+
+renamed AS (
+    SELECT
+        id AS raw_id,
+        name AS raw_name,
+        type AS raw_type,
+        context,
+        raw_data,
+        pull_date,
+        pull_timestamp
+    FROM source WHERE name = 'world bank' AND pull_date = CURRENT_DATE
+)
+
+SELECT * FROM renamed
